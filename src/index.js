f54f5389289952568c9a79809749e3b172d81da2
Aman Saxena <58439663+amans0508@users.noreply.github.com>
2020-04-21 01:32:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,20 @@ import ReactDOM from "react-dom";
 
 const element = <h1>Hello World</h1>;
 ReactDOM.render(element, document.getElementById("root"));
+
+//function inside an object is a method
+/*
+const person = {
+  name: "Aman",
+  walk: function () {
+    console.log(this);
+  },
+};
+person.walk();
+const walk = person.walk.bind(person);
+walk();
+*/
+
+const square = (number) => number * number;
+
+console.log(square(5));
