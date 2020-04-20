ba5d0361bc6a671b687398d088f39841d1adcdc8
Aman Saxena <58439663+amans0508@users.noreply.github.com>
2020-04-21 00:04:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
-// Import the React and React DOM libraries.
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function getButtonText(){
-    return 'Click on me!';
-}
-
-//Create React Components
-const App = () => {
-    //const buttonText = 'Click Me!';
-    return (
-        <div>
-            <label className ="label" for ="name">Enter Name</label>
-            <input id = "name" type ="text" />
-    <button style = {{backgroundColor: 'blue' , color : 'white' }}>{getButtonText()}</button>
-        </div>
-    );
-};
-
-//Take the react component and show it on the screen
-ReactDOM.render(
-    <App />, document.querySelector('#root')
-);
+const element = <h1>Hello World</h1>;
+ReactDOM.render(element, document.getElementById("root"));
