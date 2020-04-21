6935e284a0d55783dee3dacfdd7495156f8f3d15
Aman Saxena <58439663+amans0508@users.noreply.github.com>
2020-04-22 01:33:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const element = <h1>Hello World</h1>;
 ReactDOM.render(element, document.getElementById("root"));
 
 //function inside an object is a method
-/*
+
+/* 
 const person = {
   name: "Aman",
   walk: function () {
@@ -15,7 +16,7 @@ const person = {
 person.walk();
 const walk = person.walk.bind(person);
 walk();
-*/
+
 
 //const square = (number) => number * number;
 //console.log(square(5));
@@ -24,3 +25,25 @@ walk();
 const colors = ["red", "green", "blue"];
 const items = colors.map((color) => `<li>${color}</li>`);
 console.log(items);
+
+//Object destructuring
+//Spread operator
+const array = [1, 2, 3];
+const arraytwo = [4, 5, 6];
+
+const combined = array.concat(arraytwo);
+const combinedSpread = [...array, "1212", ...arraytwo];
+console.log("combined" + combinedSpread); */
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  walk() {
+    console.log("walk");
+  }
+}
+
+const person = new Person("Aman");
+console.log(person);
